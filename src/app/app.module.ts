43bd77203cc625511f1e3330957fa81c8b174a3e
Bianca Yamee <bianca@vexter.com.br>
2021-04-27 03:57:28 -0300

    add ionic cdn to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ClientFormsComponent } from './components/clients/client-forms/client-forms.component';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,9 +19,18 @@ import { ClientFormsComponent } from './components/clients/client-forms/client-f
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSnackBarModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule,
+    IonicModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeRoutingModule } from './home-routing.module';
 import { ClientsComponent } from '../clients/clients.component';
 import { ProductsComponent } from '../products/products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,10 +12,15 @@ import { ProductsComponent } from '../products/products.component';
     ClientsComponent,
     ProductsComponent
   ],
+  exports: [ClientsComponent,
+    ProductsComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
     RouterModule,
-  ]
+    ReactiveFormsModule,
+    FormsModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModule { }
